19c94c9c2981711bc36af03e6151679d66aed650
Anish Thapa Magar <116520964+AnishTM8@users.noreply.github.com>
2025-06-23 23:57:33 -0400

    File updated at 6/23/2025, 11:57:34 PM

--- 206-ReverseLinkedList/206-ReverseLinkedList.py
+++ 206-ReverseLinkedList/206-ReverseLinkedList.py
@@ -1,14 +1,11 @@
-# Last updated: 6/23/2025, 11:54:22 PM
-class Solution(object):
-    def containsDuplicate(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: bool
-        """
+# Last updated: 6/23/2025, 11:57:34 PM
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
         num_set = set()
 
         for num in nums:
-            if num in num_set:
+            if num not in num_set:
+                num_set.add(num)
+            else:
                 return True
-            num_set.add(num)
         return False
