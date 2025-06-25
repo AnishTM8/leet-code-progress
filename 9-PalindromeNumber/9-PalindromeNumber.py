0ac3343eb74a58f4e157b7ade21d308a8bdb3fed
Anish Thapa Magar <116520964+AnishTM8@users.noreply.github.com>
2025-06-25 09:44:29 -0400

    File updated at 6/25/2025, 9:44:29 AM

--- 9-PalindromeNumber/9-PalindromeNumber.py
+++ 9-PalindromeNumber/9-PalindromeNumber.py
@@ -1,16 +1,27 @@
-# Last updated: 6/25/2025, 9:42:55 AM
+# Last updated: 6/25/2025, 9:44:29 AM
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        if x < 0:
-            return False
+        # if x < 0:
+        #     return False
         
-        divisor = 1
-        while x >= divisor*10:
-            divisor *= 10
+        # divisor = 1
+        # while x >= divisor*10:
+        #     divisor *= 10
         
-        while x:
-            if x%10 != x//divisor:
+        # while x:
+        #     if x%10 != x//divisor:
+        #         return False
+        #     x = (x%divisor) // 10
+        #     divisor = divisor/100
+        # return True
+
+        s = str(x)
+
+        l, r = 0, len(s) - 1
+
+        while l < r:
+            if s[l] != s[r]:
                 return False
-            x = (x%divisor) // 10
-            divisor = divisor/100
+            l += 1
+            r -= 1
         return True
