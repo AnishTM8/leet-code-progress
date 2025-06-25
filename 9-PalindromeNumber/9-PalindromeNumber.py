6d517959160879116fafb2aa51de5aaaf8b79386
Anish Thapa Magar <116520964+AnishTM8@users.noreply.github.com>
2025-06-25 09:42:55 -0400

    File updated at 6/25/2025, 9:42:55 AM

--- 9-PalindromeNumber/9-PalindromeNumber.py
+++ 9-PalindromeNumber/9-PalindromeNumber.py
@@ -1,20 +1,16 @@
-# Last updated: 6/22/2025, 7:16:32 PM
-class Solution(object):
-    def isPalindrome(self, x):
-        """
-        :type x: int
-        :rtype: bool
-        """
-        if x < 0: return False
-
-        div = 1
-        while x >= 10*div: #finds the number in required for the divisor
-            div *= 10
-
-        while x:
-            if x//div != x%10: #x//div give first number and x%10 gives last number
-                return False
-            x = (x%div) // 10
-            div = div / 100
+# Last updated: 6/25/2025, 9:42:55 AM
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
         
+        divisor = 1
+        while x >= divisor*10:
+            divisor *= 10
+        
+        while x:
+            if x%10 != x//divisor:
+                return False
+            x = (x%divisor) // 10
+            divisor = divisor/100
         return True
