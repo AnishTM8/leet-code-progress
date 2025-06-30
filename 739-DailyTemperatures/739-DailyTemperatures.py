f9723bb47a6c90327a8e6ce1d144ef7d804fbf15
Anish Thapa Magar <116520964+AnishTM8@users.noreply.github.com>
2025-06-30 15:21:10 -0400

    File updated at 6/30/2025, 3:21:18 PM

--- 739-DailyTemperatures/739-DailyTemperatures.py
+++ 739-DailyTemperatures/739-DailyTemperatures.py
@@ -1,12 +1,12 @@
-# Last updated: 6/30/2025, 12:25:02 AM
+# Last updated: 6/30/2025, 3:21:18 PM
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
         res = [0] * len(temperatures)
-        stack = [] #Store Pair: [index(0), temprature(1)]
+        stack = [] # pair(i, t)
 
         for i, t in enumerate(temperatures):
-            while stack and t > stack[-1][1]: # get the top element
+            while stack and t > stack[-1][1]:
                 index, temp = stack.pop()
-                res[index] = (i - index)
+                res[index] = i - index
             stack.append([i,t])
         return res
