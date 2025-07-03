4feac521f09132f70ce668977dd79b2b8f0693cb
Anish Thapa Magar <116520964+AnishTM8@users.noreply.github.com>
2025-07-03 10:59:38 -0400

    File updated at 7/3/2025, 10:59:41 AM

--- 1768-MergeStringsAlternately/1768-MergeStringsAlternately.py
+++ 1768-MergeStringsAlternately/1768-MergeStringsAlternately.py
@@ -1,17 +1,10 @@
-# Last updated: 7/3/2025, 10:55:05 AM
-class Solution(object):
-    def mergeAlternately(self, word1, word2):
-        """
-        :type word1: str
-        :type word2: str
-        :rtype: str
-        """
-        max_len = max(len(word1), len(word2))
-        res = ""
+# Last updated: 7/3/2025, 10:59:41 AM
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
 
-        for i in range(max_len):
-            if i < len(word1):
-                res += word1[i]
-            if i < len(word2):
-                res += word2[i]
-        return res
+        if str1 + str2 != str2 + str1:
+            return ""
+        gcd = math.gcd(len(str1), len(str2))
+
+        return str1[:gcd]
+
