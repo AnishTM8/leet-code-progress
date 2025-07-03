7016325561a3d353cea408486d5517e67b93a4b4
Anish Thapa Magar <116520964+AnishTM8@users.noreply.github.com>
2025-07-03 11:18:42 -0400

    File updated at 7/3/2025, 11:18:46 AM

--- 1768-MergeStringsAlternately/1768-MergeStringsAlternately.py
+++ 1768-MergeStringsAlternately/1768-MergeStringsAlternately.py
@@ -1,10 +1,11 @@
-# Last updated: 7/3/2025, 11:07:10 AM
+# Last updated: 7/3/2025, 11:18:46 AM
 class Solution:
-    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
-        res = [False] * len(candies)
-        max_candies = max(candies)
-
-        for i in range(len(candies)):
-            if candies[i] + extraCandies >= max_candies:
-                res[i] = True
-        return res
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        for i in range(len(flowerbed)):
+            if flowerbed[i] == 0 and n!= 0:
+                prev = flowerbed[i - 1] if i > 0 else 0
+                nex = flowerbed[i + 1] if i < len(flowerbed) - 1 else 0
+                if prev == 0 and nex == 0:
+                    flowerbed[i] = 1
+                    n -= 1
+        return n<=0
