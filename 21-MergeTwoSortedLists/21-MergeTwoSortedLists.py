e4ab4a83a4a9140596d8cdde27ace8fc2e8f0415
Anish Thapa Magar <116520964+AnishTM8@users.noreply.github.com>
2025-07-02 23:21:13 -0400

    File updated at 7/2/2025, 11:21:14 PM

--- 21-MergeTwoSortedLists/21-MergeTwoSortedLists.py
+++ 21-MergeTwoSortedLists/21-MergeTwoSortedLists.py
@@ -1,4 +1,4 @@
-# Last updated: 6/22/2025, 7:16:27 PM
+# Last updated: 7/2/2025, 11:21:14 PM
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
@@ -6,22 +6,21 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        dummy = ListNode(None)
+        dummy = ListNode()
         tail = dummy
 
         while list1 and list2:
-            if list1.val < list2.val:
-                tail.next = list1
-                list1 = list1.next
-            else:
+            if list1.val > list2.val:
                 tail.next = list2
                 list2 = list2.next
+            else:
+                tail.next = list1
+                list1 = list1.next
             tail = tail.next
         
         if list1:
             tail.next = list1
-        elif list2:
+        if list2:
             tail.next = list2
         
         return dummy.next
-        
