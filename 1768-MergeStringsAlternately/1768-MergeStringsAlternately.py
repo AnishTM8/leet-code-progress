c9ea393193879c5ccf4ef84c2891ed9ac9119acf
Anish Thapa Magar <116520964+AnishTM8@users.noreply.github.com>
2025-07-03 11:28:33 -0400

    File updated at 7/3/2025, 11:28:36 AM

--- 1768-MergeStringsAlternately/1768-MergeStringsAlternately.py
+++ 1768-MergeStringsAlternately/1768-MergeStringsAlternately.py
@@ -1,18 +1,6 @@
-# Last updated: 7/3/2025, 11:27:13 AM
+# Last updated: 7/3/2025, 11:28:36 AM
 class Solution:
-    def reverseVowels(self, s: str) -> str:
-        vowels = "AEIOUaeiou"
-        s_list = list(s)
+    def reverseWords(self, s: str) -> str:
+        words = s.split()
 
-        l, r = 0, len(s) - 1
-
-        while l < r:
-            while l < r and s_list[l] not in vowels:
-                l += 1
-            while l < r and s_list[r] not in vowels:
-                r -= 1
-            s_list[l], s_list[r] = s_list[r], s_list[l]
-            l += 1
-            r -= 1
-        return "".join(s_list)
-            
+        return " ".join(words[::-1])
