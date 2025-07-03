825916b703d97db7eabf39321b193ec01712f8cf
Anish Thapa Magar <116520964+AnishTM8@users.noreply.github.com>
2025-07-03 15:12:35 -0400

    File updated at 7/3/2025, 3:12:40 PM

--- 1768-MergeStringsAlternately/1768-MergeStringsAlternately.py
+++ 1768-MergeStringsAlternately/1768-MergeStringsAlternately.py
@@ -1,6 +1,17 @@
-# Last updated: 7/3/2025, 11:28:36 AM
+# Last updated: 7/3/2025, 3:12:40 PM
 class Solution:
-    def reverseWords(self, s: str) -> str:
-        words = s.split()
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        prefix = 1
+        res = [1] * len(nums)
 
-        return " ".join(words[::-1])
+        for i in range(len(nums)):
+            res[i] = prefix
+            prefix *= nums[i]
+        
+        postfix =1
+        
+        for i in range(len(nums) - 1, -1, -1):
+            res[i] *= postfix
+            postfix *= nums[i]
+        
+        return res
