baadb26f39ec1f15bf07c6ebc910d281e9cb89d5
Anish Thapa Magar <116520964+AnishTM8@users.noreply.github.com>
2026-03-01 03:54:32 -0700

    File updated at 3/1/2026, 3:54:34 AM

--- 217-ContainsDuplicate/217-ContainsDuplicate.py
+++ 217-ContainsDuplicate/217-ContainsDuplicate.py
@@ -1,10 +1,10 @@
-# Last updated: 6/24/2025, 8:29:14 PM
-class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
-        num_set = set()
-
-        for num in nums:
-            if num in num_set:
-                return True
-            num_set.add(num)
-        return False
+# Last updated: 3/1/2026, 3:54:34 AM
+1class Solution:
+2    def containsDuplicate(self, nums: List[int]) -> bool:
+3        seen = set()
+4
+5        for i in range(len(nums)):
+6            if nums[i] in seen:
+7                return True
+8            seen.add(nums[i])
+9        return False
