d8d2a357938bd73c3c4da41f0b603a0d8deaaecd
Anish Thapa Magar <116520964+AnishTM8@users.noreply.github.com>
2026-03-03 07:27:09 -0700

    File updated at 3/3/2026, 7:27:14 AM

--- 1-TwoSum/1-TwoSum.py
+++ 1-TwoSum/1-TwoSum.py
@@ -1,13 +1,13 @@
-# Last updated: 6/26/2025, 10:20:27 AM
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        diff_map = {}
-
-        for i, num in enumerate(nums):
-            diff = target - num
-
-            if diff in diff_map:
-                return [diff_map[diff], i]
-
-            diff_map[num] = i
-        
+# Last updated: 3/3/2026, 7:27:14 AM
+1class Solution:
+2    def twoSum(self, nums: List[int], target: int) -> List[int]:
+3        seen = {}
+4
+5        for i, n in enumerate(nums):
+6            diff = target - n
+7
+8            if diff in seen:
+9                return [seen[diff], i]
+10
+11            seen[n] = i
+12        return
