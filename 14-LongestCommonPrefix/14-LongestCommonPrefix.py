424478d65839e092723b28af18a6f79e80c0004d
Anish Thapa Magar <116520964+AnishTM8@users.noreply.github.com>
2026-03-04 15:38:21 -0700

    File updated at 3/4/2026, 3:38:20 PM

--- 14-LongestCommonPrefix/14-LongestCommonPrefix.py
+++ 14-LongestCommonPrefix/14-LongestCommonPrefix.py
@@ -1,15 +1,14 @@
-# Last updated: 6/22/2025, 7:16:30 PM
-class Solution(object):
-    def longestCommonPrefix(self, strs):
-        """
-        :type strs: List[str]
-        :rtype: str
-        """
-        res = ""
-
-        for i in range(len(strs[0])):
-            for s in strs:
-                if i == len(s) or s[i] != strs[0][i]:
-                    return res
-            res += s[i]
-        return res
+# Last updated: 3/4/2026, 3:38:20 PM
+1class Solution:
+2    def longestCommonPrefix(self, strs: List[str]) -> str:
+3        res = ""
+4
+5        for i in range(len(strs[0])):
+6            for word in strs:
+7                if i == len(word) or word[i] != strs[0][i]:
+8                    return res
+9            
+10            res += strs[0][i]
+11        
+12        return res
+13
