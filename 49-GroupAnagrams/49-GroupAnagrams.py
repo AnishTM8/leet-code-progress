9ddc89e45de24bd1ca0dd230ecf968d5c9427360
Anish Thapa Magar <116520964+AnishTM8@users.noreply.github.com>
2026-03-05 17:36:29 -0700

    File updated at 3/5/2026, 5:36:30 PM

--- 49-GroupAnagrams/49-GroupAnagrams.py
+++ 49-GroupAnagrams/49-GroupAnagrams.py
@@ -1,9 +1,9 @@
-# Last updated: 6/24/2025, 8:48:28 PM
-class Solution:
-    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        res = defaultdict(list)
-
-        for s in strs:
-            sorted_s = sorted(s)
-            res[tuple(sorted_s)].append(s)
-        return list(res.values())
+# Last updated: 3/5/2026, 5:36:30 PM
+1class Solution:
+2    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+3        res = defaultdict(list)
+4
+5        for s in strs:
+6            sorted_s = sorted(s)
+7            res[tuple(sorted_s)].append(s)
+8        return list(res.values())
