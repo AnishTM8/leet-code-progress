033e5d71d1e648503305bdbb84fc965508c22517
Anish Thapa Magar <116520964+AnishTM8@users.noreply.github.com>
2026-03-06 14:02:55 -0700

    File updated at 3/6/2026, 2:02:56 PM

--- 27-RemoveElement/27-RemoveElement.py
+++ 27-RemoveElement/27-RemoveElement.py
@@ -1,10 +1,11 @@
-# Last updated: 6/22/2025, 7:16:26 PM
-class Solution:
-    def removeElement(self, nums: List[int], val: int) -> int:
-        k = 0
-
-        for i in range(len(nums)):
-            if nums[i] != val:
-                nums[k] = nums[i]
-                k += 1
-        return k
+# Last updated: 3/6/2026, 2:02:56 PM
+1class Solution:
+2    def removeElement(self, nums: List[int], val: int) -> int:
+3        k = 0
+4
+5        for i in range(len(nums)):
+6            if nums[i] != val:
+7                nums[k] = nums[i]
+8                k += 1
+9        return k
+10            
