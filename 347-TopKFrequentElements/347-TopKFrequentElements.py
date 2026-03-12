b3d316711b1bb683580f8017b9e40b2b55cfc083
Anish Thapa Magar <116520964+AnishTM8@users.noreply.github.com>
2026-03-12 17:48:09 -0600

    File updated at 3/12/2026, 5:48:11 PM

--- 347-TopKFrequentElements/347-TopKFrequentElements.py
+++ 347-TopKFrequentElements/347-TopKFrequentElements.py
@@ -1,24 +1,19 @@
-# Last updated: 6/22/2025, 7:16:10 PM
-class Solution(object):
-    def topKFrequent(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: List[int]
-        """
-        count = defaultdict(int)
-        freq = [[] for i in range(len(nums) + 1)]
-
-        for num in nums:
-            count[num] += 1
-
-        for key, value in count.items():
-            freq[value].append(key) 
-        
-        res = []
-
-        for i in range(len(freq) - 1, 0, -1):
-            for lis in freq[i]:
-                res.append(lis)
-                if len(res) == k:
-                    return res
+# Last updated: 3/12/2026, 5:48:11 PM
+1class Solution:
+2    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+3        count = {}
+4        freq = [[] for i in range(len(nums) + 1)]
+5
+6        for num in nums:
+7            count[num] = 1 + count.get(num, 0)
+8        
+9        for num, cnt in count.items():
+10            freq[cnt].append(num)
+11        
+12        res = []
+13        for i in range(len(freq) - 1, 0, -1):
+14            for num in freq[i]:
+15                res.append(num)
+16                if k == len(res):
+17                    return res
+18
