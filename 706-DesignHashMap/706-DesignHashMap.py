c6be4fa4c40a353e3063c3e30fcc247330ca4276
Anish Thapa Magar <116520964+AnishTM8@users.noreply.github.com>
2026-03-28 12:32:24 -0600

    File updated at 3/28/2026, 12:32:29 PM

--- 706-DesignHashMap/706-DesignHashMap.py
+++ 706-DesignHashMap/706-DesignHashMap.py
@@ -1,48 +1,51 @@
-# Last updated: 3/9/2026, 7:59:33 PM
+# Last updated: 3/28/2026, 12:32:29 PM
 1class ListNode:
-2    def __init__(self, key= -1, val = -1, next = None):
+2    def __init__(self, key = -1, val = -1):
 3        self.key = key
-4        self. val = val
-5        self.next = next
-6class MyHashMap:
-7
-8    def __init__(self):
-9        self.map = [ListNode() for i in range(10**4)]
-10
-11    def put(self, key: int, value: int) -> None:
-12        index = key%len(self.map)
-13        curr = self.map[index]
-14
-15        while curr.next:
-16            if curr.next.key == key:
-17                curr.next.val = value
-18                return
-19            curr = curr.next
-20        curr.next = ListNode(key, value)
+4        self.next = None
+5        self.val = val     
+6
+7class MyHashMap:
+8
+9    def __init__(self):
+10        self.map = [ListNode() for i in range(10**4)]
+11
+12    def put(self, key: int, value: int) -> None:
+13        index = key%len(self.map)
+14        curr = self.map[index]
+15
+16        while curr.next:
+17            if curr.next.key == key:
+18                curr.next.val = value
+19                return
+20            curr = curr.next
 21        
-22    def get(self, key: int) -> int:
-23        index = key%len(self.map)
-24        curr = self.map[index]
-25
-26        while curr.next:
-27            if curr.next.key == key:
-28                return curr.next.val
-29            curr = curr.next
-30        return -1
-31
-32    def remove(self, key: int) -> None:
-33        index = key%len(self.map)
-34        curr = self.map[index]
-35
-36        while curr.next:
-37            if curr.next.key == key:
-38                curr.next = curr.next.next
-39                return
-40            curr = curr.next
-41
-42
-43# Your MyHashMap object will be instantiated and called as such:
-44# obj = MyHashMap()
-45# obj.put(key,value)
-46# param_2 = obj.get(key)
-47# obj.remove(key)
+22        curr.next = ListNode(key,value)
+23        
+24
+25    def get(self, key: int) -> int:
+26        index = key%len(self.map)
+27        curr = self.map[index]
+28
+29        while curr.next:
+30            if curr.next.key == key:
+31                return curr.next.val
+32            curr = curr.next
+33        return -1        
+34
+35    def remove(self, key: int) -> None:
+36        
+37        index = key%len(self.map)
+38        curr = self.map[index]
+39
+40        while curr.next:
+41            if curr.next.key == key:
+42                curr.next = curr.next.next
+43                return
+44            curr = curr.next
+45
+46# Your MyHashMap object will be instantiated and called as such:
+47# obj = MyHashMap()
+48# obj.put(key,value)
+49# param_2 = obj.get(key)
+50# obj.remove(key)
