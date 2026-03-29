ecf0a7ff01fe820db382084cdd3d1f168018e4c0
Anish Thapa Magar <116520964+AnishTM8@users.noreply.github.com>
2026-03-29 10:14:52 -0600

    File updated at 3/29/2026, 10:14:52 AM

--- 75-SortColors/75-SortColors.py
+++ 75-SortColors/75-SortColors.py
@@ -1,23 +1,19 @@
-# Last updated: 3/11/2026, 8:27:15 PM
+# Last updated: 3/29/2026, 10:14:52 AM
 1class Solution:
 2    def sortColors(self, nums: List[int]) -> None:
 3        """
 4        Do not return anything, modify nums in-place instead.
 5        """
-6        def swap(i, j):
-7            nums[i], nums[j] = nums[j], nums[i]
+6        l, r = 0, len(nums) - 1
+7        k = 0
 8
-9        l, r = 0, len(nums) - 1
-10        k = 0
-11
-12        while k <= r:
-13            if nums[k] == 0:
-14                swap(l, k)
-15                l += 1
-16            elif nums[k] == 2:
-17                swap(k, r)
-18                r -= 1
-19                k -= 1
-20            k += 1
-21        
-22
+9        while k <= r:
+10            if nums[k] == 0:
+11                nums[l],nums[k] = nums[k], nums[l]
+12                l += 1
+13            elif nums[k] == 2:
+14                nums[r], nums[k] = nums[k], nums[r]
+15                r -= 1
+16                k -= 1
+17            k += 1
+18        
