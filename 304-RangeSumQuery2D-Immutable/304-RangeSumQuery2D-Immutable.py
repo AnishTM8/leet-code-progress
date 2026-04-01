d4bdb94783aeef0a7ad73f14c3a337ec4e2fbd41
Anish Thapa Magar <116520964+AnishTM8@users.noreply.github.com>
2026-03-31 19:45:18 -0600

    File updated at 3/31/2026, 7:45:20 PM

--- 304-RangeSumQuery2D-Immutable/304-RangeSumQuery2D-Immutable.py
+++ 304-RangeSumQuery2D-Immutable/304-RangeSumQuery2D-Immutable.py
@@ -1,10 +1,10 @@
-# Last updated: 3/31/2026, 7:43:37 PM
+# Last updated: 3/31/2026, 7:45:20 PM
 1class NumMatrix:
 2
 3    def __init__(self, matrix: List[List[int]]):
-4        ROWS, COLS = len(matrix), len(matrix[0])
-5        self.sumMat = [[0] * (COLS + 1) for r in range(ROWS + 1)]
-6
+4        COLS, ROWS = len(matrix[0]), len(matrix)
+5        self.sumMat = [[0]*(COLS + 1) for i in range(ROWS  + 1)]
+6        
 7        for r in range(ROWS):
 8            prefix = 0
 9            for c in range(COLS):
@@ -15,12 +15,13 @@
 14    def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
 15        r1, c1, r2, c2 = row1 + 1, col1 + 1, row2 + 1, col2 + 1
 16        bottomRight = self.sumMat[r2][c2]
-17        left = self.sumMat[r2][c1 - 1]
-18        topLeft = self.sumMat[r1 - 1][c1 - 1]
-19        above = self.sumMat[r1 - 1][c2]
-20
-21        return bottomRight - above - left + topLeft
+17        above = self.sumMat[r1 - 1][c2]
+18        left = self.sumMat[r2][c1 - 1]
+19        topLeft = self.sumMat[r1 - 1][c1 - 1]
+20        return bottomRight - above - left + topLeft
+21
 22
-23# Your NumMatrix object will be instantiated and called as such:
-24# obj = NumMatrix(matrix)
-25# param_1 = obj.sumRegion(row1,col1,row2,col2)
+23
+24# Your NumMatrix object will be instantiated and called as such:
+25# obj = NumMatrix(matrix)
+26# param_1 = obj.sumRegion(row1,col1,row2,col2)
