7e59b91012d088b68e03aad165f458ae895979f8
Anish Thapa Magar <116520964+AnishTM8@users.noreply.github.com>
2026-04-06 17:56:04 -0600

    File updated at 4/6/2026, 5:56:06 PM

--- 229-MajorityElementII/229-MajorityElementII.py
+++ 229-MajorityElementII/229-MajorityElementII.py
@@ -1,25 +1,25 @@
-# Last updated: 4/6/2026, 5:55:12 PM
+# Last updated: 4/6/2026, 5:56:06 PM
 1class Solution:
 2    def majorityElement(self, nums: List[int]) -> List[int]:
 3        count = defaultdict(int)
 4
-5        for num in nums:
-6            count[num] += 1 
+5        for n in nums:
+6            count[n] += 1
 7
 8            if len(count) <= 2:
 9                continue
 10            
 11            new_count = defaultdict(int)
-12            for n,c in count.items():
-13                if c > 1:
-14                    new_count[n] = c - 1
-15            
-16            count = new_count
-17        
-18        res = []
-19
+12
+13            for n,c in count.items():
+14                if c > 1:
+15                    new_count[n] = c - 1
+16            
+17            count = new_count
+18        
+19        res = []
 20        for n in count:
-21            if nums.count(n) > len(nums)//3:
+21            if nums.count(n) > len(nums) // 3:
 22                res.append(n)
-23        return res
-24                
+23        
+24        return res
